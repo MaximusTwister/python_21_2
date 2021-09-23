5003696b2ee5f970c459d50cb36f131f281adb58
Artem Vdovyk <artem.vdovyk@gmail.com>
2021-09-23 21:22:14 +0300

    Homework 15

---
+++ vdovyk/lesson_15/homework_15/constants.py
@@ -0,0 +1,10 @@
+CATEGORY_OF_EXPENSES = ['Food', 'Bills', 'Transportation', 'Home', 'Car', 'Entertainment', 'Shopping',
+                        'Clothing', 'Insurance', 'Tax', 'Telephone', 'Health', 'Sport', 'Baby', 'Pet',
+                        'Beauty', 'Electronics', 'Hamburger', 'Wine', 'Vegetables', 'Snacks', 'Gift',
+                        'Social', 'Travel', 'Education', 'Fruits', 'Book', 'Office', 'Other']
+
+ACCOUNTS = ['MonoBank', 'PrivatBank', 'Cash', 'WifeCash']
+
+DEFAULT_CURRENCY = "$"
+
+CATEGORY_OR_INCOME = ['Rabota', 'Rental', 'Deposit', 'Freelance']
